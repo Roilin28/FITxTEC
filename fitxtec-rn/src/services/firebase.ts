476d94cdd5657b7bbb0a62f1d4bea30ac9820b3f
IRojas44 <isasctopz@gmail.com>
2025-10-05 17:23:52 -0600

    Login implementation

---
+++ fitxtec-rn/src/services/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGfupRcSBp-lszXz8onMQEHI3V-rYzT0U",
+  authDomain: "fitxtec.firebaseapp.com",
+  projectId: "fitxtec",
+  storageBucket: "fitxtec.firebasestorage.app",
+  messagingSenderId: "152331365997",
+  appId: "1:152331365997:web:8693846f3fa36870555964"
+};
+
+let app: FirebaseApp;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0]!;
+}
+
+export const db = getFirestore(app);
